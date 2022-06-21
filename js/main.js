f1a1e259d6a9b6f726b2d225dcc61a23996e97a3
David <davidkch@yandex.ru>
2022-06-22 02:39:33 +0300

    Исправил замечания, убрал избыточные функции и откорректировал названия.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
+// Функция для получения случайного целого числа из переданного диапазона включительно.
 function getRandomNumber (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomTitle() {
-  const titles = ['Выгодное предложение!', 'Последний щанс!', 'Title3', 'Title4'];
-  return getRandomData(titles);
+//функция для получения массива случайной длины с возможностью повторения элементов.
+function getNewRandomArray(arr) {
+  const newRandomArray = [];
+  for (let i = 0; i <= getRandomNumber(0, 10); i++) {
+    newRandomArray.push(arr[getRandomNumber(0, arr.length - 1)]);
+  }
+  return newRandomArray;
 }
 
-function getRandomType() {
-  const types = ['Hotel ', 'Motel','B&B','Guest house','Extended Stay Hotel'];
-  return  getRandomData(types);
-}
-
-function getRandomFeatures() {
-  const features = ['wifi', 'parking', 'dishwasher', 'washer', 'elevator', 'conditioner'];
-  const randomIndex = getRandomNumber(0, features.length - 1);
-  return features.slice(0, randomIndex);
-}
-
+// Функция взята из интернета и доработана.
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 function getRandomLocationPart(a, b, digits = 5 )
 {
   const lower = Math.min(Math.abs(a), Math.abs(b));
@@ -28,33 +24,31 @@ function getRandomLocationPart(a, b, digits = 5 )
   return +result.toFixed(digits);
 }
 
-function getRandomPositiveNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+// Функция для получения случайного элемента массива
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
-function getRandomChekTime() {
-  const time = ['12:00', '13:00', '14:00'];
-  return getRandomData(time);
-}
-
+// Функция для получения значений latitude и longitude.
 function getRandomLocation() {
   return { lat: getRandomLocationPart(35.65000, 35.70000, 5), lng: getRandomLocationPart(139.70000, 139.80000, 5) };
 }
 
-function getRandomData(data) {
-  const randomIndex = getRandomNumber(0, data.length - 1);
-  return data[randomIndex];
-}
 
+// Функция для создания массива из сгенерированных JS-объектов.
 function generateOffers() {
   const offers = [];
 
   for (let i = 1; i <= 10; i++) {
+    const features = ['wifi', 'parking', 'dishwasher', 'washer', 'elevator', 'conditioner'];
+    const titles = ['Выгодное предложение!', 'Последний щанс!', 'Title3', 'Title4'];
+    const time = ['12:00', '13:00', '14:00'];
+    const types = ['Hotel ', 'Motel', 'B&B', 'Guest house', 'Extended Stay Hotel'];
+    const photos = [
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+    ];
     const rooms = getRandomNumber(1, 6);
     const location = getRandomLocation();
-
 
     const offer = {
       author: {
@@ -64,17 +58,19 @@ function generateOffers() {
       location,
 
       offer: {
-        title: getRandomTitle(),
-        description: 'Гостиница содержит много комфортабельных номеров!',
-        price: getRandomPositiveNumber(200, 500),
-        type: getRandomType(),
-        rooms: getRandomPositiveNumber(1, 6),
-        guests: rooms * getRandomNumber(1, 2),
-        checkin: getRandomChekTime(),
-        checkout: getRandomChekTime(),
-        features: getRandomFeatures(),
+        title: getRandomArrayElement(titles),
         address: `${location.lat} , ${location.lng}`,
+        price: getRandomNumber(200, 500),
+        type: getRandomArrayElement(types),
+        rooms: getRandomNumber(1, 6),
+        guests: rooms * getRandomNumber(1, 2),
+        checkin: getRandomArrayElement(time),
+        checkout: getRandomArrayElement(time),
+        features: getNewRandomArray(features),
+        description: 'Гостиница содержит много комфортабельных номеров!',
+        photos: getNewRandomArray(photos),
       },
+
     };
 
     offers.push(offer);
